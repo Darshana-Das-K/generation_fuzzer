39360af958fd82c82811cdad1f9714ae90e840bb
Darshana-Das-K <darshanadask@gmail.com>
2025-05-26 15:22:04 +0800

    Added simple_header in notebook.

--- generation_fuzzer_main.py
+++ generation_fuzzer_main.py
@@ -42,38 +42,23 @@ def write_leaf_values_to_file(data_tree, output_directory):
                 #repeat_field(data_tree, output_directory, field_value, 1)
             #value = item.get('expansion')
             
-#def generation_fuzzer(ksy_file):
-output_directory = 'testcases'
-os.makedirs(output_directory, exist_ok=True)
-# Specify the path to your YAML file
-file_path = '/Users/darshanadask/main_project/generation_fuzzer_final_phase/ksy_files/glibc.ksy'
+def generate_binary(ksy_file, output_directory):
+    os.makedirs(output_directory, exist_ok=True)
 
-# Read the YAML data from the file
-with open(file_path, 'r') as file:  
-    yaml_data = file.read()
+    with open(ksy_file, 'r') as file:
+        yaml_data = file.read()
 
-# Load YAML data
-data_tree = yaml.safe_load(yaml_data)
-#print("2704: ", data_tree['seq']['types']['data_descriptor'])
-#print(data_tree)
-#for i in range(0, 100):
-endianness, file_extension,id = handle_meta(data_tree['meta'])
+    data_tree = yaml.safe_load(yaml_data)
 
-#dependency_graph= preprocess_kaitai_struct(data_tree['seq'])
-#print("Dependency tree is: ",dependency_graph )
+    endianness, file_extension, file_id = handle_meta(data_tree['meta'])
 
-#ordered_list= dependency_order(dependency_graph)
-#print("data_tree is:", data_tree)
-#expansion = handle_seq(data_tree['seq'], endianness, data_tree)
-handle_seq(data_tree['seq'], endianness, data_tree, data_tree, 'data_tree')
-print("Final answer:  ",data_tree)
-#handle_instances(data_tree['instances'], endianness, data_tree, data_tree, None)
-#print(data_tree)
-# Determine the file path
-#filename = f"{id}.{file_extension}" if file_extension else f"{id}"
-#filepath = os.path.join(output_directory, filename)
-generated_file_path= write_leaf_values_to_file(data_tree, output_directory)
-#return generated_file_path
-# Write the output to the file
-#with open(filepath, 'wb') as file:
-#    file.write(expansion)
+    # Optional: dependency analysis (can be printed if needed)
+    # dependency_graph = preprocess_kaitai_struct(data_tree['seq'])
+    # ordered_list = dependency_order(dependency_graph)
+
+    handle_seq(data_tree['seq'], endianness, data_tree, data_tree, 'data_tree')
+
+    # Optional: Uncomment if instances should also be handled
+    # handle_instances(data_tree['instances'], endianness, data_tree, data_tree, None)
+
+    return write_leaf_values_to_file(data_tree, output_directory)
